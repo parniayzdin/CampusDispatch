8d46ac56652861b63c2748279f28fecf40cb9869
Parnia Yazdinia <pari.yazdinia@gmail.com>
2025-11-09 14:10:05 -0500

    Added comments to db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,8 +2,10 @@ import os
 import psycopg
 from contextlib import asynccontextmanager
 
+#Load the database connection URL from environment variables
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+#Define an async context for database connections
 @asynccontextmanager
 async def get_conn():
     async with await psycopg.AsyncConnection.connect(DATABASE_URL) as conn:
