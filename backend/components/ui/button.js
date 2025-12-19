ae2777826392e676931e457eeba99e6e9fed366d
Parnia Yazdinia <pari.yazdinia@gmail.com>
2025-12-19 18:00:40 -0500

    Updated button.js

--- backend/components/ui/button.js
+++ backend/components/ui/button.js
@@ -14,3 +14,5 @@ export const buttonVariants = cva(
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
